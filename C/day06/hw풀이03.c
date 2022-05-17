314c1991f87d6afa2841ff0e16ac1af3a6e7101a
Jcrystal <gomdorij@naver.com>
2022-05-17 14:51:36 +0900

    .

--- C/day06/hw풀이03.c
+++ C/day06/hw풀이03.c
@@ -1,21 +1,22 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+#include <stdio.h>
 void main(void)
 {
 	int days[13] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 	int total;
 	printf("1 - 365 : ");
 	scanf("%d", &total);
+
 	int month = 1;
 	int day = total;
 
-	while(day > days[month]);
+	while (day > days[month])
 	{
 		day = day - days[month];
 		++month;
 	}
 
-	printf("1/1 + %d老 = %d/%d/\n", total, month, day)
-	// 100老 => (1岿 + 2岿 + 3岿) + 10老
+	printf("1/1 + %d老 = %d/%d\n", total, month, day);
 }
